ec1e4e6c5a187e498179e6c5c8471e9492b9f21a
Joan Nekoye <joannekoye@gmail.com>
2020-05-10 20:54:50 +0300

    set up db and db access structure + models in models.py

---
+++ manage.py
@@ -0,0 +1,16 @@
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User, Blog, Comment, PhotoProfile
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Blog = Blog, Comment=Comment, PhotoProfile=PhotoProfile)
+
+if __name__ == '__main__':
+    manager.run()
