579e1b6e5f9a670dfdad2ac71a1cd8abcaabcf56
Joan Nekoye <joannekoye@gmail.com>
2020-05-10 16:17:32 +0300

    set up api request in request.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    QUOTES_API_URL='http://quotes.stormconsultancy.co.uk/random.json'
 
 
 
---
+++ app/models/quote.py
@@ -0,0 +1,8 @@
+class Quote:
+    '''
+    Quote class to hold random quote
+    '''
+
+    def __init__(self, author, quote):
+        self.author = author
+        self.quote = quote
